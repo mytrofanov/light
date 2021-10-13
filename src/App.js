c085d96ee887eadd87fe4c82504ccb6d071adefb
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-13 18:36:16 +0300

    фильтр работает

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+function App() {
+
+
+    let text = `In this text you should highlight word text`
+    let text2 = ['aaaa', 'bbbb', 'cccc', 'text', 'word']
+    let searchText = 'b'
+    // let resultText = `<mark>${searchText}</mark>`
+    // let regularExpression = new RegExp(searchText, 'gi')
+    // text = text.replace(regularExpression, resultText)
+    let textHighlighter;
+    return (
+        <div className="App">
+            <div className="text" id='text' key='text'>
+
+                {text2.map((result, i) => {
+                    let searchKeywordIdx = result.indexOf(searchText);
+                    if (searchKeywordIdx > -1) {
+                        textHighlighter = [
+                            result.substring(0, searchKeywordIdx),
+                            <strong key={i}>
+                                {result.substring(searchKeywordIdx, searchKeywordIdx + searchText.length)}
+                            </strong>,
+                            result.substring(searchKeywordIdx + searchText.length)
+                        ];
+                    }
+                    return <li key={result}>{textHighlighter}</li>;
+                })}
+
+
+                </div>
+                </div>
+                )}
+
+
+                    export default App;
