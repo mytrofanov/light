54f4f3a4adeda8f70c439d9e09c0da41876ca57f
mytrofanov <mytrofanovmaxym@gmail.com>
2021-10-15 08:43:24 +0300

    Skeleton

--- src/App.js
+++ src/App.js
@@ -6,10 +6,20 @@ function App() {
 
 
     return (
-<div>
+<div className='frame'>
+<div className='screen'>
+<div id='Minutes' className='time'>00</div>
+<div id='Seconds' className='time' >00</div>
+<div id='Miliseconds' className='time'>00</div>
 
+</div>
 
+<div className='buttons'>
+<button>Start/Stop</button>
+<button>Wait</button>
+<button>Reset</button>
 
+</div>
 
         </div>
 )}
